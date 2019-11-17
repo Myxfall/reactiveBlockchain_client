5961034bdde88434058f43b58a394264e46f1a5e
Myxfall <maximilienromain96@hotmail.com>
2019-11-17 17:30:14 +0100

    Refactor front to include diplomas and grades information from the blockchain

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,49 +4,65 @@ import 'rxjs/add/operator/delay';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message: string;
-  messages: string[] = [];
-  counter;
-  bntStyle: string;
+    message: string;
+    messages: string[] = [];
+    counter;
+    bntStyle: string;
+    currentMessageCounter;
+    currentDiv: HTMLElement;
+
+    diplomas: string[] = [];
+    grades: string[] = [];
 
 
-  constructor(private chatService: ChatService) {
-      this.counter = 0;
-      this.bntStyle = 'btn-default';
+    constructor(private chatService: ChatService) {
+        this.counter = 0;
+        this.bntStyle = 'btn-default';
 
-  }
+    }
 
-  sendMessage() {
-      var message_json = {
-          message: this.message,
-      }
-    this.chatService.sendMessage(message_json);
-    //this.chatService.sendMessage(this.message);
-    this.messages.push(this.message);
+    sendMessage() {
+        var message_json = {
+            message: this.message,
+        }
 
-     const myElement: HTMLElement = document.getElementById("testId");
-    myElement.innerHTML += "Worked";
- 	myElement.style.background="blue";
+        this.messages.push(this.message);
+        this.chatService.sendMessage(message_json);
+        this.currentMessageCounter = this.messages.length - 1;
 
 
-    this.bntStyle = 'btn-default';
-    this.message = '';
-    this.counter = this.counter + 1;
+        const myElement: HTMLElement = document.getElementById("testId");
+        myElement.innerHTML += " Worked";
+        myElement.style.background="blue";
 
-  }
 
-  ngOnInit() {
-    this.chatService
-      .getMessages().delay(1000)
-      .subscribe((message: string) => {
-        this.messages.push(message);
-        this.bntStyle = 'btn-change';
 
-      });
-  }
+        this.bntStyle = 'btn-default';
+        this.message = '';
+        this.counter = this.counter + 1;
+
+    }
+
+    ngOnInit() {
+        this.chatService
+        .getMessages().delay(1000)
+        .subscribe((data) => {
+            console.log(`Received value from the server with ${data}`);
+
+            const data_record = data.record;
+            switch (data_record.type) {
+                case 'diploma':
+                    this.diplomas.push(`${data_record.last_name} ${data_record.first_name} got ${data_record.study} diploma from the ${data_record.school}`);
+                    break;
+                case 'grade':
+                    this.grades.push(`The student ${data_record.last_name} with netID ${data_record.username} got ${data_record.grade} for the course ${data_record.course}`);
+            }
+            this.bntStyle = 'btn-change';
+        });
+    }
 }
